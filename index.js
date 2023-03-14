fca8aab86b6270c1efd6b04849011577985a3bf3
Codeprof Computer Academy <info@codeprof.net>
2023-03-14 17:52:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -42,37 +42,57 @@ let courses = [
     },
 ]
 
+
 // menu btn
 let menu_btn = document.querySelector('.menu');
 let navbar = document.querySelector('.navbar');
 let main = document.querySelector('main');
 
-
-// search input
-let search_input = document.querySelector('.search-input')
-
-// search button
-let search_btn = document.querySelector('.search-btn')
-
-search_btn.addEventListener('click', (event)=>{
-    event.preventDefault()
-
-//    courses.find((course)=>{
-//         if(course.course_code === search_input.value){
-//             alert("Coming Soon 😊")
-//         }
-
-//     })
-
-alert("Coming Soon 😊")
-
-
-})
-
 menu_btn.addEventListener('click', (event)=>{
     event.preventDefault();
     navbar.classList.toggle('slide-in')
     main.classList.toggle('main-adjust')
+
+})
+
+// targetting the close btn, result display, search-btn, the search-input-field,.
+let close_btn = document.querySelector('.close-btn');
+let result_display = document.querySelector('.result-display')
+let search_btn = document.querySelector('.search-btn')
+let search_input = document.querySelector('.search-input')
+
+let id = document.querySelector('.id')
+let code = document.querySelector('.code')
+let title = document.querySelector('.title')
+let lecturer = document.querySelector('.lecturer')
+let result = document.querySelector('.score')
+let remark = document.querySelector('.remark')
+
+
+close_btn.addEventListener('click', ()=>{
+     result_display.style.display = "none"
+})
+
+// search and display result logic
+search_btn.addEventListener('click', ()=>{
+      let search_value = search_input.value;
+
+
+    //   searching through the course db
+    courses.filter((course)=>{
+          if(course.course_code === search_value){
+
+               id.innerHTML = course.id
+               code.innerHTML = course.course_code
+               title.innerHTML = course.course_title
+               lecturer.innerHTML = course.lecturer
+               result.innerHTML = course.result
+               remark.innerHTML = course.Remarks
+          }
+    })
+
+    result_display.style.display = "flex"
+       
 
 })
 
